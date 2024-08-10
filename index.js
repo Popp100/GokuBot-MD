5d33bb84eb632b7b8fc98a73b0de5a3e5d11b8af
Spellblade Sorcerer <121673607+Popp100@users.noreply.github.com>
2024-08-09 20:44:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Goku\nBot-MD', {
+say('Issei\nBot-MD', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • Jostin207`, {
+say(`Developed By • Belmont`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
